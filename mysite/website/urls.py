fdbaba28bfb62efa4e8d507fc37486a43b83f939
simbyungki <dung8524@gmail.com>
2020-12-11 17:40:08 +0900

    update

--- mysite/website/urls.py
+++ mysite/website/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
 	path('', views.news_list, name='news_list'),
 	path('news_list', views.news_list, name='news_list'),
 	path('reload_data', views.reload_data, name='reload_data'),
-	path('login', views.login, name='login'),
-	path('logout', views.logout, name='logout'),
-	path('join', views.join, name='join'),
+	path('login/', views.login, name='login'),
+	path('logout/', views.logout, name='logout'),
+	path('join/', views.join, name='join'),
 	path('list_data/', views.list_data, name='list_data'),
 ]
