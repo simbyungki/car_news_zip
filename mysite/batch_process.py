ec38b98be482c63d13884831500b90c3f6402371
simbyungki <dung8524@gmail.com>
2021-01-19 15:40:25 +0900

    update

--- mysite/batch_process.py
+++ mysite/batch_process.py
@@ -2073,21 +2073,21 @@ if __name__ == '__main__' :
 	print('실행')
 	# load_detail_data()
 	# reload_list_data()
-	run_text_mining()
-	# # Schedule Work
-	# # 매일 5회 (오전 9시 / 오후 12시 / 오후 3시 / 오후 6시 / 오후 10시) 뉴스 데이터 수집
-	# schedule.every().days.at('09:00').do(reload_list_data)
-	# schedule.every().days.at('12:00').do(reload_list_data)
-	# schedule.every().days.at('15:00').do(reload_list_data)
-	# # schedule.every().days.at('18:00').do(reload_list_data)
-	# schedule.every().days.at('22:00').do(reload_list_data)
+	# run_text_mining()
+	# Schedule Work
+	# 매일 5회 (오전 9시 / 오후 12시 / 오후 3시 / 오후 6시 / 오후 10시) 뉴스 데이터 수집
+	schedule.every().days.at('09:00').do(reload_list_data)
+	schedule.every().days.at('12:00').do(reload_list_data)
+	schedule.every().days.at('15:00').do(reload_list_data)
+	schedule.every().days.at('18:00').do(reload_list_data)
+	schedule.every().days.at('22:00').do(reload_list_data)
 
-	# # 매일 1회 (오전 01시) 뉴스 본문 데이터 수집
-	# schedule.every().days.at('01:00').do(load_detail_data)
+	# 매일 1회 (오전 01시) 뉴스 본문 데이터 수집
+	schedule.every().days.at('01:00').do(load_detail_data)
 
-	# # 매일 1회 (오전 05시) 뉴스 본문 데이터 분석
-	# schedule.every().days.at('17:26').do(run_text_mining)
+	# 매일 1회 (오전 05시) 뉴스 본문 데이터 분석
+	schedule.every().days.at('15:45').do(run_text_mining)
 
-	# while True :
-	# 	schedule.run_pending()
-	# 	time.sleep(1)
+	while True :
+		schedule.run_pending()
+		time.sleep(1)
